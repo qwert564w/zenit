c317547084b939e4dc1bcd9fdbc751bf5fb415a2
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-28 11:37:54 +0300

    cleanup(Backtrack): strip unused star-imports

--- src/main/java/org/zenith/module/combat/Backtrack.java
+++ src/main/java/org/zenith/module/combat/Backtrack.java
@@ -3,13 +3,6 @@ package org.zenith.module.combat;
 import org.zenith.module.Category;
 import org.zenith.module.Module;
 import org.zenith.module.ModuleInfo;
-import org.zenith.module.ModuleManager;
-import org.zenith.module.combat.*;
-import org.zenith.module.movement.*;
-import org.zenith.module.player.*;
-import org.zenith.module.render.*;
-import org.zenith.module.misc.*;
-
 
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import com.darkmagician6.eventapi.EventTarget;
@@ -279,7 +272,6 @@ public final class Backtrack extends Module {
    public Vec3d float272() {
       return this.vec3d34;
    }
-
 
    public static class Service {
       public final Packet<?> packet3;
