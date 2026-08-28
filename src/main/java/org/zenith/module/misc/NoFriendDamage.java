ad011ec84f0906c3c8f4037946a4178438f78556
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-28 11:39:20 +0300

    cleanup(NoFriendDamage): strip unused star-imports

--- src/main/java/org/zenith/module/misc/NoFriendDamage.java
+++ src/main/java/org/zenith/module/misc/NoFriendDamage.java
@@ -3,12 +3,6 @@ package org.zenith.module.misc;
 import org.zenith.module.Category;
 import org.zenith.module.Module;
 import org.zenith.module.ModuleInfo;
-import org.zenith.module.ModuleManager;
-import org.zenith.module.combat.*;
-import org.zenith.module.movement.*;
-import org.zenith.module.player.*;
-import org.zenith.module.render.*;
-import org.zenith.module.misc.*;
 
 @ModuleInfo(name = "NoFriendDamage", category = Category.MISC, description = "Дает рейкасту проходить сквозь друзей")
 public final class NoFriendDamage extends Module {
