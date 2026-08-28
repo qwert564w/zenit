4318e42a40e974d02b51203b258c90b662b4400f
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-28 11:39:15 +0300

    cleanup(InventorySetting): strip unused star-imports

--- src/main/java/org/zenith/module/misc/InventorySetting.java
+++ src/main/java/org/zenith/module/misc/InventorySetting.java
@@ -3,12 +3,6 @@ package org.zenith.module.misc;
 import org.zenith.module.Category;
 import org.zenith.module.Module;
 import org.zenith.module.ModuleInfo;
-import org.zenith.module.ModuleManager;
-import org.zenith.module.combat.*;
-import org.zenith.module.movement.*;
-import org.zenith.module.player.*;
-import org.zenith.module.render.*;
-import org.zenith.module.misc.*;
 
 import org.zenith.setting.BooleanSetting;
 
