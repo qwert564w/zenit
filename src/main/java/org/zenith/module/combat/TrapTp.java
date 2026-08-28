945b17900ce6219b0553f7bb55fd78e0233b7bb4
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-28 11:38:02 +0300

    cleanup(TrapTp): strip unused star-imports

--- src/main/java/org/zenith/module/combat/TrapTp.java
+++ src/main/java/org/zenith/module/combat/TrapTp.java
@@ -3,12 +3,6 @@ package org.zenith.module.combat;
 import org.zenith.module.Category;
 import org.zenith.module.Module;
 import org.zenith.module.ModuleInfo;
-import org.zenith.module.ModuleManager;
-import org.zenith.module.combat.*;
-import org.zenith.module.movement.*;
-import org.zenith.module.player.*;
-import org.zenith.module.render.*;
-import org.zenith.module.misc.*;
 
 import com.darkmagician6.eventapi.EventTarget;
 import com.mojang.blaze3d.platform.DestFactor;
