296bc8a337f5b684774212104595d17df3a54464
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-28 11:39:27 +0300

    cleanup(ShulkerPreview): strip unused star-imports

--- src/main/java/org/zenith/module/misc/ShulkerPreview.java
+++ src/main/java/org/zenith/module/misc/ShulkerPreview.java
@@ -3,12 +3,6 @@ package org.zenith.module.misc;
 import org.zenith.module.Category;
 import org.zenith.module.Module;
 import org.zenith.module.ModuleInfo;
-import org.zenith.module.ModuleManager;
-import org.zenith.module.combat.*;
-import org.zenith.module.movement.*;
-import org.zenith.module.player.*;
-import org.zenith.module.render.*;
-import org.zenith.module.misc.*;
 
 import com.darkmagician6.eventapi.EventTarget;
 import java.util.Optional;
@@ -198,7 +192,6 @@ public class ShulkerPreview extends Module {
 
       return defaultedlist;
    }
-
 
    public static final class ShulkerTooltipData implements TooltipData {
       public final DefaultedList<ItemStack> value;
