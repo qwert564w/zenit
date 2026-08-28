a4800893d3a5046a156cadb2a407d38dfc2c57f2
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-28 11:38:48 +0300

    cleanup(AutoAuth): strip unused star-imports

--- src/main/java/org/zenith/module/misc/AutoAuth.java
+++ src/main/java/org/zenith/module/misc/AutoAuth.java
@@ -3,13 +3,6 @@ package org.zenith.module.misc;
 import org.zenith.module.Category;
 import org.zenith.module.Module;
 import org.zenith.module.ModuleInfo;
-import org.zenith.module.ModuleManager;
-import org.zenith.module.combat.*;
-import org.zenith.module.movement.*;
-import org.zenith.module.player.*;
-import org.zenith.module.render.*;
-import org.zenith.module.misc.*;
-
 
 import com.google.gson.JsonObject;
 import com.darkmagician6.eventapi.EventTarget;
@@ -153,7 +146,6 @@ public final class AutoAuth extends Module {
          }
       }
    }
-
 
    public static class TransientPasswordSetting extends TextSetting {
       public final AutoAuth val504;
