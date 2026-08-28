b613ee8d22dc958339b924e3f0c38775aae0b46d
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-28 11:38:49 +0300

    cleanup(AutoCapcha): strip unused star-imports

--- src/main/java/org/zenith/module/misc/AutoCapcha.java
+++ src/main/java/org/zenith/module/misc/AutoCapcha.java
@@ -3,12 +3,6 @@ package org.zenith.module.misc;
 import org.zenith.module.Category;
 import org.zenith.module.Module;
 import org.zenith.module.ModuleInfo;
-import org.zenith.module.ModuleManager;
-import org.zenith.module.combat.*;
-import org.zenith.module.movement.*;
-import org.zenith.module.player.*;
-import org.zenith.module.render.*;
-import org.zenith.module.misc.*;
 
 import com.darkmagician6.eventapi.EventTarget;
 import java.awt.Color;
@@ -695,7 +689,6 @@ public final class AutoCapcha extends Module {
          }
       });
    }
-
 
    public record ImageRegion(BufferedImage bufferedImage, int int145, int int146, int int147, int int148) {
       public BufferedImage image() {
